44a9700aedc3ae7bfacd043cbc2dacbf4feb394d
Rami Al-Dhafiri <raldhafiri@users.noreply.github.com>
2024-07-07 12:18:00 +0300

    Update open_filex.dart

--- lib/open_filex.dart
+++ lib/open_filex.dart
@@ -5,4 +5,4 @@ export 'src/common/open_result.dart';
 
 /// Use platform specific codes or use web if it's the web
 export 'src/platform/open_filex.dart'
-    if (dart.library.html) 'src/web/open_filex.dart';
+    if (dart.library.js_interop) 'src/web/open_filex.dart';
